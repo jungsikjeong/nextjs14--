0c7b987541988e89b82a9c37a5428ceb0c882c13
jungsikjeong <wndtlr1024@gmail.com>
2024-09-14 11:14:12 +0900

    root-layout 컴포넌트분리

--- foodies-starting-project/components/main-header/index.js
+++ foodies-starting-project/components/main-header/index.js
@@ -3,26 +3,30 @@ import Link from 'next/link';
 
 import logoImg from '@/assets/logo.png';
 import classes from './main-header.module.css';
+import MainHeaderBackground from './main-header-background';
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href='/'>
-        {/* priority는 로딩 우선처리임 이거없으면 경고 뜸 */}
-        <Image src={logoImg} alt='A plate with food on it' priority />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href='/'>
+          {/* priority는 로딩 우선처리임 이거없으면 경고 뜸 */}
+          <Image src={logoImg} alt='A plate with food on it' priority />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href='/meals'>Browse Meals</Link>
-          </li>
-          <li>
-            <Link href='/community'>Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href='/meals'>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href='/community'>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
