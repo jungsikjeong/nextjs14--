d18a6837e7c729eb92d735cca1de4705fdaf761d
jungsikjeong <wndtlr1024@gmail.com>
2024-09-14 17:33:57 +0900

    에러 컴포넌트

--- foodies-starting-project/lib/meals.js
+++ foodies-starting-project/lib/meals.js
@@ -4,5 +4,6 @@ const db = sql('meals.db');
 
 export async function getMeals() {
   await new Promise((resolver) => setTimeout(resolver, 2000)); // 추가지연임
+  // throw new Error('Loading meals failed');
   return db.prepare('SELECT * FROM meals').all();
 }
