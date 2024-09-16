3cb2fefc9a32b760d6f278ca52a5aec96bf41fad
jungsikjeong <wndtlr1024@gmail.com>
2024-09-16 21:23:23 +0900

    다이나믹 라우트 안에 중첩된 라우트

--- Routing&PageRendering/app/news/[id]/page.js
+++ Routing&PageRendering/app/news/[id]/page.js
@@ -1,3 +1,26 @@
-export default function NewsDetailPage() {
-  return <div>NewsDetailPage</div>;
+import { DUMMY_NEWS } from '@/dummy-news';
+import Link from 'next/link';
+import { notFound } from 'next/navigation';
+
+export default function NewsDetailPage({ params }) {
+  const newsId = params.id;
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsId);
+
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <article className='news-article'>
+      <header>
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date}</time>
+      </header>
+
+      <p>{newsItem.content}</p>
+    </article>
+  );
 }
