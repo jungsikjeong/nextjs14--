59a300de726a00ca3ce1566f776c2c6cd761ff23
jungsikjeong <wndtlr1024@gmail.com>
2024-09-16 12:11:03 +0900

    nextjs cache기능인 revalidatePath를 사용. 캐시 유효성 재확인 트리거기능.

--- foodies-starting-project/lib/actions.js
+++ foodies-starting-project/lib/actions.js
@@ -2,6 +2,7 @@
 
 import { redirect } from 'next/navigation';
 import { saveMeal } from './meals';
+import { revalidatePath } from 'next/cache';
 
 function isInvalidText(text) {
   return !text.title || text.title.trim() === '';
@@ -33,5 +34,8 @@ export async function shareMeal(preveState, formData) {
   }
 
   await saveMeal(meal);
+  // nextjs가 해당 페이지에 연관된 캐싱된 캐시들을 비움
+  revalidatePath('/meals', 'page'); // 디폴트 값으로 '/meals' 페이지만 유효성을 재검사한다는 뜻
+  // revalidatePath('/meals', 'layout'); // layout로해두면 중첩된 모든 페이지를 재검사함
   redirect('/meals');
 }
