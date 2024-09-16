020a47d3aa40d705302eeb6042206c0801623961
jungsikjeong <wndtlr1024@gmail.com>
2024-09-16 20:01:31 +0900

    Catch-All 라우트 여러 경로 세그먼트 처리

--- Routing&PageRendering/app/archive/@archive/[[...filter]]/page.js
+++ Routing&PageRendering/app/archive/@archive/[[...filter]]/page.js
@@ -1,28 +1,59 @@
 import NewsList from '@/components/news-list';
-import { getAvailableNewsYears, getNewsForYear } from '@/lib/news';
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from '@/lib/news';
 import Link from 'next/link';
 import React from 'react';
 
 export default function FilteredNewsPage({ params }) {
   const filter = params.filter;
-  console.log(filter); // 2024, 2023,2022 헤더 목록이있으면 2024를 누르면 콘솔에 ['2024'] 가 찍힘
+  // console.log(filter); // 2024, 2023,2022 헤더 목록이있으면 2024를 누르면 콘솔에 ['2024'] 가 찍힘
 
-  const links = getAvailableNewsYears();
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+
+  let news;
+  let links = getAvailableNewsYears();
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  let newsContent = <p>선택된 기간에대한 뉴스를 찾지 못했습니다.</p>;
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
-    <header id='archive-header'>
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
-  // const news = getNewsForYear(newsYear);
+    <>
+      <header id='archive-header'>
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
 
-  // return <NewsList news={news} />;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
 }
