9793651ff520947ac40795dcc3fa6e5b47710656
jungsikjeong <wndtlr1024@gmail.com>
2024-09-17 15:13:39 +0900

    모달 바깥클릭시 router.back으로 이전페이지로 되돌리기

--- Routing&PageRendering/app/news/[slug]/@modal/(.)image/page.js
+++ Routing&PageRendering/app/news/[slug]/@modal/(.)image/page.js
@@ -1,13 +1,16 @@
-import { notFound } from 'next/navigation';
+'use client';
+
+import { notFound, useRouter } from 'next/navigation';
 
 import { DUMMY_NEWS } from '@/dummy-news';
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
   );
-  console.log(newsItem);
 
   if (!newsItem) {
     notFound();
@@ -15,7 +18,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className='modal-backdrop' />
+      <div className='modal-backdrop' onClick={router.back} />
       <dialog className='modal' open>
         <div className='fullscreen-image'>
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
