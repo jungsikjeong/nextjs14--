3a43fa54eeaec6b58a2fe2dcebb9c1e550e381b4
jungsikjeong <wndtlr1024@gmail.com>
2024-09-17 15:41:13 +0900

    api 라우트 핸들러

---
+++ Routing&PageRendering/api/route.js
@@ -0,0 +1,10 @@
+// route는 다양한 함수를 내보내는 함수임 EX) GET,POST, PUT, DELETE..
+
+export function GET(req) {
+  console.log(re);
+
+  // return Response.json();
+  return new Response('Hello!');
+}
+
+// export function POST(req) {}
