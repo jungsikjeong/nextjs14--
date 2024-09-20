c846402b80c11f417305cce668adcfc11e76486c
jungsikjeong <wndtlr1024@gmail.com>
2024-09-20 19:41:03 +0900

    데이터 캐싱 제어 방법들..

--- 캐싱 이해 및 구성하기/app/messages/page.js
+++ 캐싱 이해 및 구성하기/app/messages/page.js
@@ -1,16 +1,20 @@
+import { unstable_noStore } from 'next/cache';
+
 import Messages from '@/components/messages';
 
+// export와 revalidate이름을 꼭 명시해줘야됌
+// export const revalidate = 5; // 5초동안 데이터가 캐시되어 재사용됌 fetch의 next:{revalidate} 옵션과 같음
+
+// export const dynamic ='auto' // 기본값
+// export const dynamic = 'force-dynamic' // fetch요청에서 캐시 설정을 no-store로 설정한것과 같음
+// export const dynamic = 'force-static' // 캐싱처리된 데이터만 가져옴, 새로운 데이터 X
+
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    next: {
-      revalidate: 5, // 5초마다 데이터 새롭게 생성
-    },
-    // cache: 'force-cache', // 캐시된 데이터를 강제로 사용, 네트워크 요청을 보내지 않고, 캐시에 있는 데이터를 반환함,default값임
-    // cache: 'no-store', // 캐시를 사용하지 않고, 항상 새로운 데이터를 가져옴. 요청할 때마다 서버에서 최신 데이터를 받아옴
-    // cache: 'no-cache', // 서버에 요청을 보내지만, 캐시를 검증함. 서버에서 최신 데이터를 가져올지, 캐시된 데이터를 사용할지 결정
-    // cache: 'only-if-cached', // 네트워크 요청을 보내지 않고, 캐시에 저장된 데이터가 있을 때만 사용. 오프라인 상태에서 주로 사용됨
-    // cache: 'reload', // 서버에서 항상 최신 데이터를 가져옴 기존의 캐시된 데이터를 무시하고 새로 데이터를 요청함.
-  });
+  // export const dynamic = 'force-dynamic'와 같음, 하지만 더 나은 방식임
+  // 데이터가 캐시되지 않도록 확실하게 하고자 하는 컴포넌트 내에서 호출한다.
+  // 이 컴포넌트 또는 이 컴포넌트로부터 보내질 수 있는 모든 데이터 소스에대한 요청에 대해 얻게된다.
+  unstable_noStore();
+  const response = await fetch('http://localhost:8080/messages');
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
