2414ccee4ee124c71563e70455f848f980a3d21a
jungsikjeong <wndtlr1024@gmail.com>
2024-09-21 15:49:29 +0900

    이미지 width,height, sizes, proiority 옵션등등 설명

--- nextjs 앱 최적화/components/header.js
+++ nextjs 앱 최적화/components/header.js
@@ -1,11 +1,21 @@
 import logo from '@/assets/logo.png';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Header() {
+  console.log(logo);
   return (
     <header id='main-header'>
       <Link href='/'>
-        <img src={logo.src} alt='Mobile phone with posts feed on it' />
+        <Image
+          src={logo}
+          // 고정된 이미지 크기가 주어진 사용 사례에 경우엔 width,height를 사용.
+          width={100}
+          height={100}
+          // sizes='10vw' // 현재 사용하는 뷰포트의 대략 10%의 너비로 조정된다는 의미
+          priority // 페이지가 로드될 때 화면에 무조건 표시될 것을 알고있다면 이 옵션을 주자., 이미지 사전로딩 , 지연로딩 비활성화
+          alt='Mobile phone with posts feed on it'
+        />
       </Link>
       <nav>
         <ul>
