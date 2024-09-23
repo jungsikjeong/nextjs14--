dca1abf19460932c7b6c23ceaa0a56129e4d82a3
jungsikjeong <wndtlr1024@gmail.com>
2024-09-23 16:18:14 +0900

    searchParams를 활용해 쿼리 mode에 따라서 로그인, 회원가입 폼 변환

--- 사용자 가입 또는 로그인/components/auth-form.js
+++ 사용자 가입 또는 로그인/components/auth-form.js
@@ -5,7 +5,9 @@ import { useFormState } from 'react-dom';
 
 import { signup } from '@/actions/auth-actions';
 
-export default function AuthForm() {
+export default function AuthForm({ mode }) {
+  // 'login' or 'signup'
+
   const [formState, formAction] = useFormState(signup, {});
   return (
     <form id='auth-form' action={formAction}>
@@ -30,10 +32,18 @@ export default function AuthForm() {
       )}
 
       <p>
-        <button type='submit'>Create Account</button>
+        <button type='submit'>
+          {mode === 'login' ? 'Lohin' : 'Create Account'}
+        </button>
       </p>
+
       <p>
-        <Link href='/'>Login with existing account.</Link>
+        {mode === 'login' && (
+          <Link href='/?mode=signup'>Create an account.</Link>
+        )}
+        {mode === 'signup' && (
+          <Link href='/?mode=login'>Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
